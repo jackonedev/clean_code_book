5d90b91861561aa92845ac92dd932d6f6fcd789a
jackonedev <jackone.action.software@gmail.com>
2022-09-19 15:05:36 -0300

    Finalizado logger para Data Descriptor

--- descriptors_3.py
+++ descriptors_3.py
@@ -5,14 +5,20 @@ logger, msg = buildLogger('descriptors_3')
 
 class DataDescriptor:
 
+    def __init__(self):
+        self._name = None
+
     def __get__(self, obj, objtype):
         if obj is None:
             return self
         return 42
 
+    def __set_name__(self, objtype, name):
+        self._name = name
+
     def __set__(self, obj, value):
         logger.info(msg.set(f"setting {obj}.descriptor to {value}"))
-        obj.__dict__["descriptor"] = value
+        obj.__dict__[self._name] = value
 
 class ClientClass:
     descriptor = DataDescriptor()
@@ -22,6 +28,12 @@ def main():
     client = ClientClass()
     client.descriptor
     print (client.descriptor)
+
+    client.descriptor = 99
+    print (client.descriptor)
+
+    print (vars(client))
+    print (client.__dict__["descriptor"])
 
 
 if __name__ == '__main__':
