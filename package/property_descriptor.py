9a7a4825fdcfb6e0a552a0180ee04fe35cf777be
jackonedev <jackone.action.software@gmail.com>
2022-09-20 12:11:51 -0300

    Property Descritpor finished

--- package/property_descriptor.py
+++ package/property_descriptor.py
@@ -3,3 +3,35 @@ Do not implement a descriptor unless there is actual evidence of
 the repetition we are trying to solve, and the complexity is proven
 to have paid off.
 """
+
+class HistoryTracedAttribute:
+    def __init__(self, trace_attribute_name: str) -> None:
+        self.trace_attribute_name = trace_attribute_name
+        self._name = None
+
+    def __set_name__(self, owner, name):
+        self._name = name
+
+    def __get__(self, instance, owner):
+        if instance is None:
+            return self
+        return instance.__dict__[self._name]
+
+    def __set__(self, instance, value):
+        self._track_change_in_value_for_instance(instance, value)
+        instance.__dict__[self._name] = value
+
+    def _track_change_in_value_for_instance(self, instance, value):
+        self._set_default(instance)
+        if self._needs_to_track_change(instance, value):
+            instance.__dict__[self.trace_attribute_name].append(value)
+
+    def _needs_to_track_change(self, instance, value) -> bool:
+        try:
+            current_value = instance.__dict__[self._name]
+        except KeyError:
+            return True
+        return value != current_value
+
+    def _set_default(self, instance):
+        instance.__dict__.setdefault(self.trace_attribute_name, [])
