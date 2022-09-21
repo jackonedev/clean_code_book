632c76d7336650e4c1641a068a10e92ec6375a94
jackonedev <jackone.action.software@gmail.com>
2022-09-21 07:25:20 -0300

    Cambiando variables a convención personal

--- package/property_descriptor.py
+++ package/property_descriptor.py
@@ -9,29 +9,29 @@ class HistoryTracedAttribute:
         self.trace_attribute_name = trace_attribute_name
         self._name = None
 
-    def __set_name__(self, owner, name):
+    def __set_name__(self, objtype, name):
         self._name = name
 
-    def __get__(self, instance, owner):
-        if instance is None:
+    def __get__(self, obj, objtype):
+        if obj is None:
             return self
-        return instance.__dict__[self._name]
+        return obj.__dict__[self._name]
 
-    def __set__(self, instance, value):
-        self._track_change_in_value_for_instance(instance, value)
-        instance.__dict__[self._name] = value
+    def __set__(self, obj, value):
+        self._track_change_in_value_for_instance(obj, value)
+        obj.__dict__[self._name] = value
 
-    def _track_change_in_value_for_instance(self, instance, value):
-        self._set_default(instance)
-        if self._needs_to_track_change(instance, value):
-            instance.__dict__[self.trace_attribute_name].append(value)
+    def _track_change_in_value_for_instance(self, obj, value):
+        self._set_default(obj)
+        if self._needs_to_track_change(obj, value):
+            obj.__dict__[self.trace_attribute_name].append(value)
 
-    def _needs_to_track_change(self, instance, value) -> bool:
+    def _needs_to_track_change(self, obj, value) -> bool:
         try:
-            current_value = instance.__dict__[self._name]
+            current_value = obj.__dict__[self._name]
         except KeyError:
             return True
         return value != current_value
 
-    def _set_default(self, instance):
-        instance.__dict__.setdefault(self.trace_attribute_name, [])
+    def _set_default(self, obj):
+        obj.__dict__.setdefault(self.trace_attribute_name, [])
