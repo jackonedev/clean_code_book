660ea16f2dbc9b940275a6d655d4e7811d785fc9
jackonedev <jackone.action.software@gmail.com>
2022-09-21 10:51:31 -0300

    Actualizado a formato PEP8

--- package/explore_object.py
+++ package/explore_object.py
@@ -1,14 +1,17 @@
 def check_dir(obj):
-    print ("="*90, f"\nPRINTING attr's FROM {obj.__class__}\n")
+    print("=" * 90, f"\nPRINTING attr's FROM {obj.__class__.__name__}\n")
     for attr_name in dir(obj):
         attr_value = getattr(obj, attr_name)
-        msg = "> {}:\n{} | Callable:{}".format(attr_name, attr_value, callable(attr_value))
+        msg = "> {}:\n{} | Callable:{}".format(
+            attr_name, attr_value, callable(attr_value)
+        )
         print(msg)
-    print("="*90, "\n")
+    print("=" * 90, "\n")
 
 
 def check_vars(obj):
     from pprint import pprint
-    print ("="*90, f"\nPRINTING vars({obj.__class__})\n")
-    print(vars(obj))
-    print("="*90, "\n")
+
+    print("=" * 90, f"\nPRINTING vars({obj.__class__.__name__}.instance)\n")
+    pprint(vars(obj))
+    print("=" * 90, "\n")
